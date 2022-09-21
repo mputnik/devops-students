be2cc65b5fc338aba827d1c39087fff42df5a429
Jerry Soong <60661834+RoboticJ4000@users.noreply.github.com>
2022-09-21 19:11:19 -0400

    Got GC theme to work

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main property="mainContentOfPage" class="container" resource="#wb-main" typeof="WebPageElement">
+        <h1>Static header and footer - Bootstrap 3 - Update to header to come...</h1>
+        <p>Static header and footer on Bootstrap 3 with the mega menu. Web Experience Toolkit (WET) includes reusable components for building and maintaining innovative Web sites that are accessible, usable, and interoperable.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut tortor ligula. Proin in maximus arcu. Nam sit amet dolor ac magna aliquam lacinia non in ante. Nam eu aliquet est, et consectetur est. Sed rhoncus orci sit amet felis imperdiet, at finibus tortor ultrices. Etiam porttitor elementum diam vel auctor. Suspendisse sagittis, urna et blandit placerat, quam neque dignissim ligula, id malesuada sapien enim id erat. Sed sapien turpis, iaculis vitae risus vel, luctus placerat lorem. Nunc egestas nibh a pretium luctus. Pellentesque vitae pretium ante. Nam elementum orci quis quam aliquet, at consequat turpis ullamcorper. Fusce libero risus, dapibus sed lacinia non, porta congue nulla. Sed ac tortor ut erat volutpat sollicitudin. Phasellus condimentum neque a quam posuere condimentum.</p>
+        <p>Nulla tempus odio fringilla dui egestas, sed hendrerit nunc tempor. Donec ultricies nulla leo, eget fringilla libero lobortis tristique. Curabitur pellentesque luctus odio, vitae semper ex. Integer posuere sem est, in hendrerit sem cursus ut. Aliquam felis felis, sollicitudin eu tincidunt non, egestas et orci.</p>
+        <p><a href="bootstrap-4.html" class="btn btn-primary">Switch to Bootstrap 4 (experimental)</a></p>
+      </main>
     </div>
   );
 }
