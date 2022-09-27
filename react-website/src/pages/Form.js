b06e874d1ec73bd05a3913d408e7c5f123cf6aac
Justinzoorob <justinzoorob@hotmail.com>
2022-09-27 13:41:46 -0400

    Update Form.js
    updated form layout

--- react-website/src/pages/Form.js
+++ react-website/src/pages/Form.js
@@ -5,40 +5,33 @@ function Form () {
         <>
             <form property="mainContentOfPage" class="container" resource="#wb-main" typeof="WebPageElement">
                 <h1>Personal Information Form</h1>
-                <div class="form-group col-lg-4 col-md-4 col-sm-12">
+                <div class="form-group col-lg-12 col-md-12 col-sm-12">
                     <label class="control-label required" for="Fname">First Name</label>
-                    <input type="text" class="form-control" id="Fname" size="35"/>
+                    <input type="text" class="form-control" id="Fname" size="40"/>
                 </div>
-                <div class="form-group col-lg-4 col-md-4 col-sm-12">
+                <div class="form-group col-lg-12 col-md-12 col-sm-12">
                     <label class="control-label required" for="Lname">Last Name</label>
-                    <input type="text" class="form-control" id="Lname" size="35"/>
+                    <input type="text" class="form-control" id="Lname" size="40"/>
                 </div>
-                <div class="form-group col-lg-4 col-md-4 col-sm-12">
+                <div class="form-group col-lg-12 col-md-12 col-sm-12">
                     <label class="control-label required" for="Colour">Colour</label>
-                    <input type="text" class="form-control" id="Colour" size="35"/>
+                    <input type="text" class="form-control" id="Colour" size="40"/>
                 </div>
-                <div class="form-group col-lg-6 col-md-6 col-sm-12">
-                    <fieldset class="gc-chckbxrdio">
-                    <legend class="required">Which is your favorite pet?</legend>
-                        <ul class="list-unstyled lst-spcd-2">
-                            <li class="radio">
-			                    <input type="radio" name="animal" id="Dog"/>
-			                    <label for="Dog">Dog</label>
-		                    </li>
-                            <li class="radio">
-			                    <input type="radio" name="animal" id="Cat"/>
-			                    <label for="Cat">Cat</label>
-		                    </li>
-                        </ul>
-                    </fieldset>
+                <div class="form-group col-lg-12 col-md-12 col-sm-12">
+                    <label class="control-label required" for="Colour">Pet Preference</label>
+                    <select class="form-control" id="Colour">
+                        <option>Dog</option>
+                        <option>Cat</option>
+                    </select>
                 </div>
-                <div className="form-group">
-                    <textarea class="form-control" rows={5} cols={50} id="feedback" placeholder="Add message for cookie.">
-                        This is simple textarea
-                    </textarea>
+                <div className="form-group col-lg-12 col-md-12 col-sm-12">
+                    <label class="control-label" for="feedback">Nice Message</label>
+                    <textarea class="form-control" rows={5} cols={50} id="feedback" placeholder="Add message for cookie."></textarea>
+                    <br></br>
+                    <button type="submit" class="btn btn-default">Submit</button>
                 </div>
-                <button type="submit" class="btn btn-default">Submit</button>
             </form>
+            <br></br>
         </>
     )
 }
