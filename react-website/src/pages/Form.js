9cc72c12bfcee540a8932f5e67fa5969f1b03ce0
Dweevil <andrewrmackinnon@cmail.carleton.ca>
2022-09-28 11:37:10 -0400

    Added form page details

--- react-website/src/pages/Form.js
+++ react-website/src/pages/Form.js
@@ -7,21 +7,22 @@ function Form () {
                 <h1>Personal Information Form</h1>
                 <div class="form-group col-lg-12 col-md-12 col-sm-12">
                     <label class="control-label required" for="Fname">First Name</label>
-                    <input type="text" class="form-control" id="Fname" size="40"/>
+                    <input type="text" class="form-control" id="Fname" size="40" placeholder="John"/>
                 </div>
                 <div class="form-group col-lg-12 col-md-12 col-sm-12">
                     <label class="control-label required" for="Lname">Last Name</label>
-                    <input type="text" class="form-control" id="Lname" size="40"/>
+                    <input type="text" class="form-control" id="Lname" size="40" placeholder="Doe"/>
                 </div>
                 <div class="form-group col-lg-12 col-md-12 col-sm-12">
                     <label class="control-label required" for="Colour">Colour</label>
-                    <input type="text" class="form-control" id="Colour" size="40"/>
+                    <input type="text" class="form-control" id="Colour" size="40" placeholder="Red"/>
                 </div>
                 <div class="form-group col-lg-12 col-md-12 col-sm-12">
                     <label class="control-label required" for="Colour">Pet Preference</label>
                     <select class="form-control" id="Colour">
                         <option>Dog</option>
                         <option>Cat</option>
+                        <option>Fish</option>
                     </select>
                 </div>
                 <div className="form-group col-lg-12 col-md-12 col-sm-12">
