e535d9827eb77ccab19c6add2b7b0654b4caa9cf
Justinzoorob <justinzoorob@hotmail.com>
2022-09-29 14:33:41 -0400

    Added confirmation.test.js
    added test('should render return to homepage link')

--- react-website/src/pages/Confirmation.js
+++ react-website/src/pages/Confirmation.js
@@ -6,8 +6,8 @@ function Confirmation() {
             <main property="mainContentOfPage" class="container" resource="#wb-main" typeof="WebPageElement">
                 <h1>Confirmation</h1>
                 <p>The form has been successfully submitted. Your submission is now saved in the database.</p>
-                <p>You can view your entry alongside others on the table page <Link to="/data">here</Link>.</p>
-                <p><Link to="/" className="btn btn-primary">Return to home page</Link></p>
+                <p>You can view your entry alongside others on the table page <Link to="/placeholder">here</Link>.</p>
+                <p data-testid={'return'}><Link to="/data" className="btn btn-primary">Return to home page</Link></p>
                 <br /> <br /> <br />
             </main>
         </>
