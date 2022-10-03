1bf2186fe5e1141b1500020ac4e55538255ddf8c
Khalid Kanaan <khalid.kanaan.ca@gmail.com>
2022-10-03 17:28:30 -0400

    established connection to mongodb database using mongoose, added log message to check if connection to database was successful, defined database schema and model using mongoose, saved data to mongodb database

--- react-website/server.js
+++ react-website/server.js
@@ -11,6 +11,49 @@ const app = express();
 // Define port
 const PORT = process.env.PORT || 8080;
 
+// Establish connection to mongodb database
+mongoose.connect('mongodb://0.0.0.0:27017/form_data', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// mongoose listener to check connection
+mongoose.connection.on('connected', () => {
+    console.log('Connection to mongodb database established successfully.')
+});
+
+// mongodb schema
+const Schema = mongoose.Schema;
+
+const FormPostSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    favoritePet: String,
+    favoriteColor: String,
+    message: String
+});
+
+// register a Model by passing schema
+const FormPost = mongoose.model('FormPost', FormPostSchema);
+
+// Save data to mongodb database
+const data = {
+    firstName: 'Khalid',
+    lastName: 'Kanaan',
+    favoritePet: 'cat',
+    favoriteColor: 'green',
+    message: 'Hello world!'
+};
+
+const newFormPost = new FormPost(data); // creating an instance of the model
+newFormPost.save((error) => {
+    if (error) {
+        console.log(`Could not save data to mongodb database ${error.message}`);
+    }else{
+        console.log('Data successfuly saved to mongodb database');
+    }
+});
+
 // Automatic HTTP request logger
 app.use(morgan('combined'));
 
