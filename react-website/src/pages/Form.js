c26d9dbc9fd456c5641fba38371ca42a75a3f673
Justinzoorob <justinzoorob@hotmail.com>
2022-10-04 11:16:52 -0400

    Added 2 new tests
    Added 2 tests:
    - Confirmation.test.js
    - Form.test.js

--- react-website/src/pages/Form.js
+++ react-website/src/pages/Form.js
@@ -13,27 +13,27 @@ function Form () {
         <>
             <form onSubmit={handleSubmit} property="mainContentOfPage" class="container" resource="#wb-main" typeof="WebPageElement">
                 <h1>Personal Information Form</h1>
-                <div class="form-group col-lg-12 col-md-12 col-sm-12">
+                <div data-testid={'FnameInput'}  class="form-group col-lg-12 col-md-12 col-sm-12">
                     <label class="control-label required" for="Fname">First Name</label>
-                    <input type="text" class="form-control" id="Fname" size="40" placeholder="John"/>
+                    <input type="text" class="form-control" id="Fname" size="40" placeholder="John"></input>
                 </div>
-                <div class="form-group col-lg-12 col-md-12 col-sm-12">
+                <div data-testid={'LnameInput'} class="form-group col-lg-12 col-md-12 col-sm-12">
                     <label class="control-label required" for="Lname">Last Name</label>
-                    <input type="text" class="form-control" id="Lname" size="40" placeholder="Doe"/>
+                    <input type="text" class="form-control" id="Lname" size="40" placeholder="Doe"></input>
                 </div>
-                <div class="form-group col-lg-12 col-md-12 col-sm-12">
+                <div data-testid={'ColourInput'} class="form-group col-lg-12 col-md-12 col-sm-12">
                     <label class="control-label required" for="Colour">Colour</label>
-                    <input type="text" class="form-control" id="Colour" size="40" placeholder="Red"/>
+                    <input type="text" class="form-control" id="Colour" size="40" placeholder="Red"></input>
                 </div>
-                <div class="form-group col-lg-12 col-md-12 col-sm-12">
-                    <label class="control-label required" for="Colour">Pet Preference</label>
-                    <select class="form-control" id="Colour">
+                <div data-testid={'PetSelect'} class="form-group col-lg-12 col-md-12 col-sm-12">
+                    <label class="control-label required" for="pet">Pet Preference</label>
+                    <select class="form-control" id="pet">
                         <option>Dog</option>
                         <option>Cat</option>
                         <option>Fish</option>
                     </select>
                 </div>
-                <div className="form-group col-lg-12 col-md-12 col-sm-12">
+                <div data-testid={'SubmitBtn'} className="form-group col-lg-12 col-md-12 col-sm-12">
                     <label class="control-label" for="feedback">Nice Message</label>
                     <textarea class="form-control" rows={5} cols={50} id="feedback" placeholder="Add message for cookie."></textarea>
                     <br></br>
