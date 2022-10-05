95b9e5c47c9bedd5054758e63ab858e9776b4383
Dweevil <andrewrmackinnon@cmail.carleton.ca>
2022-10-05 12:48:14 -0400

    fixes links on confirmation page

--- react-website/src/pages/Confirmation.js
+++ react-website/src/pages/Confirmation.js
@@ -6,7 +6,7 @@ function Confirmation() {
             <main property="mainContentOfPage" className="container" resource="#wb-main" typeof="WebPageElement">
                 <h1>Confirmation</h1>
                 <p>The form has been successfully submitted. Your submission is now saved in the database.</p>
-                <p>You can view your entry alongside others on the table page <Link to="/data">here</Link>.</p>
+                <p>You can view your entry alongside others on the table page <a href="/data">here</a>.</p>
                 <p><Link to="/" className="btn btn-primary">Return to home page</Link></p>
                 <br /> <br /> <br />
             </main>
