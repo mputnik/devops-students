b5a896239ae638c08a90396f60c78fd1f0b2f4cc
Jerry Soong <60661834+jjsoong@users.noreply.github.com>
2022-10-21 17:07:03 -0400

    Improved image_build.sh - removed unnecessary cd lines - added relational pathing to build commands

--- dev-scripts/image_build.sh
+++ dev-scripts/image_build.sh
@@ -1,10 +1,6 @@
 # Builds the three (react, api, proxy) docker images, while setting up the docker network and opening the mongodb container.
 sudo docker network create --driver=bridge dnet
-cd ../react-website/
-sudo docker build -t react .
-cd ../server/
-sudo docker build -t api .
-cd ../proxy/
-sudo docker build -t proxy .
-cd ..
+sudo docker build -t react ../react-website/
+sudo docker build -t api ../server/
+sudo docker build -t proxy ../proxy/
 sudo docker run -d --network=dnet --name database mongo:latest
