72fda3b4b82b080d6ea9e040e81f4e9e89536d18
Astrid <andrew.mackinnon@hc-sc.gc.ca>
2022-10-25 16:19:54 -0400

    Added admin sign in page

---
+++ react-website/src/pages/AdminSignIn.js
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+function AdminSignIn() {
+    const navigate = useNavigate();
+    
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+    
+    const [userErr, setUserErr] = useState(false);
+    const [passwordErr, setPasswordErr] = useState(false);
+    
+    function handleSubmit (event) {
+        event.preventDefault();
+        
+        setUserErr(user === "");
+        setPasswordErr(password === "");
+
+        if(user !== "" && password !== ""){
+            const body = {
+                user: user,
+                password: password,
+            };
+
+            // axios({
+            //     // Full url defined in as proxy in package.json 
+            //     url: '/api/save',
+            //     method: 'POST',
+            //     data: body
+            // })
+            //     .then(()=>{
+            //         navigate("/confirmation");
+            //         console.log('The form data was successfuly sent to the server');
+            //     })
+            //     .catch((error)=>{
+            //         console.log('Internal server error: could not send form data to the server');
+            //     });
+        }
+
+    }
+    return (
+        <>
+            <form id='form' onSubmit={(e) => handleSubmit(e)} property="mainContentOfPage" className="container" resource="#wb-main" typeof="WebPageElement">
+                <h1>Enter Username & Password</h1>
+                <div data-testid={'UserInput'} className="form-group col-lg-12 col-md-12 col-sm-12">
+                    <label className="control-label required" htmlFor="User">Username <strong className="required">(required)</strong></label>
+                    {userErr && <ErrorMsg htmlFor="User"/>}
+                    <input type="text" className="form-control" id="User" size="40" placeholder="" value={user} onChange={(e) => setUser(e.target.value)}/>
+                </div>
+                <div data-testid={'PasswordInput'} className="form-group col-lg-12 col-md-12 col-sm-12">
+                    <label className="control-label required" htmlFor="Password">Password <strong className="required">(required)</strong></label>
+                    {passwordErr && <ErrorMsg htmlFor="Password"/>}
+                    <input type="text" className="form-control" id="Password" size="40" placeholder="" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                <div data-testid={'SubmitBtn'} className="form-group col-lg-12 col-md-12 col-sm-12">
+                    <br></br>
+                    <button type="submit" className="btn btn-default">Sign In </button>
+                </div>
+            </form>
+            <br></br>
+        </>
+    );
+}
+function ErrorMsg(props) {
+    return (
+        <>
+            <br/>
+            <label className="control-label" htmlFor={props.htmlFor}>
+            <strong id="title1-error" className="error"><span className="label label-danger"><span className="prefix">Error: </span>This field is required.</span></strong>
+            </label>
+        </>
+    )
+}
+export default AdminSignIn;
